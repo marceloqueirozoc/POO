1f5a97a971a9f19cf0cf7104d22303d61d361b7e
Marcelo Constantino <marceloqueirozoc@gmail.com>
2020-09-15 18:53:50 -0300

    Inicio

---
+++ prjconversao2/src/com/mconstantino/Main.java
@@ -0,0 +1,19 @@
+package com.mconstantino;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a,b,soma;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o valor de A: ");
+        a = input.nextInt();
+        System.out.println("Digite o valor de A: ");
+        b = input.nextInt();
+        soma = a + b;
+        System.out.println("Soma de valores: " + soma);
+
+    }
+}
